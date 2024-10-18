3163c5e6a9ede53f2dc422359638f9945ba3f9f3
GiYoung <rlduddl5519@naver.com>
2024-10-18 17:39:18 +0900

    10-18 first commit

---
+++ src/main/java/com/example/demo/mapper/StoreInfoMapper.java
@@ -0,0 +1,19 @@
+package com.example.demo.mapper;
+
+import com.example.demo.vo.StoreInfoVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Mapper
+public interface StoreInfoMapper {
+
+    void insertItems(HashMap<String, Object> item);
+
+    List<StoreInfoVO> selList();
+
+    //해쉬맵 넣기
+    List<HashMap<String, Object>> selectShopByShopName(String keyword);
+}
---
+++ src/main/java/com/example/demo/service/CrudService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.vo.StoreVO;
+
+public interface CrudService<E> {
+	List<E> selectList(E e);
+	
+	E selectOne(E e);
+	
+	void insert(E e);
+	
+	void update(E e);
+	
+	void delete(E e);
+
+	List<StoreVO> selectAll();
+}
